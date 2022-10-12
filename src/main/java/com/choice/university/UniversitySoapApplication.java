ede76f9cc02990a7f536cfc5e9c00eee6029ffe2
Ricardo Barajas <ricardo.barajas@encora.com>
2022-10-12 15:37:51 -0500

    chore: Initial commit

---
+++ src/main/java/com/choice/university/UniversitySoapApplication.java
@@ -0,0 +1,12 @@
+package com.choice.university;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversitySoapApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UniversitySoapApplication.class, args);
+  }
+}
---
+++ src/test/java/com/choice/university/UniversitySoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choice.university;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversitySoapApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
