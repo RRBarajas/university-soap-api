1deb072e138dc11ae673bde48295a93ee9b3a5b4
Ricardo Barajas <ricardo.barajas@encora.com>
2022-10-19 14:16:32 -0500

    chore: Adding request schema validation

--- src/main/java/com/choice/university/config/WebServiceConfig.java
+++ src/main/java/com/choice/university/config/WebServiceConfig.java
@@ -2,6 +2,7 @@ package com.choice.university.config;
 
 import static com.choice.university.constants.UniversityConstants.UNIVERSITY_NAMESPACE_URI;
 
+import java.util.List;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.EndpointInterceptor;
+import org.springframework.ws.soap.server.endpoint.interceptor.PayloadValidatingInterceptor;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
@@ -40,5 +43,13 @@ public class WebServiceConfig extends WsConfigurerAdapter {
   @Bean
   public XsdSchema hotelsSchema() {
     return new SimpleXsdSchema(new ClassPathResource("schemas/hotels.xsd"));
+  }
+
+  @Override
+  public void addInterceptors(List<EndpointInterceptor> interceptors) {
+    var validatingInterceptor = new PayloadValidatingInterceptor();
+    validatingInterceptor.setValidateRequest(true);
+    validatingInterceptor.setXsdSchema(hotelsSchema());
+    interceptors.add(validatingInterceptor);
   }
 }
